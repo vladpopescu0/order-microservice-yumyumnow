23ec30906e93fc8037093973c3a0891e40ce2d3e
Petra Gulyas <p.gulyas-1@student.tudelft.nl>
2024-01-05 14:24:45 +0100

    Testing OrderService: createOrder

--- order-microservice/src/test/java/nl/tudelft/sem/template/order/domain/user/OrderServiceTests.java
+++ order-microservice/src/test/java/nl/tudelft/sem/template/order/domain/user/OrderServiceTests.java
@@ -1,5 +1,6 @@
 package nl.tudelft.sem.template.order.domain.user;
 
+import nl.tudelft.sem.template.order.commons.Address;
 import nl.tudelft.sem.template.order.commons.Order;
 import nl.tudelft.sem.template.order.domain.user.repositories.OrderRepository;
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +13,8 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,8 +29,28 @@ class OrderServiceTests {
     @InjectMocks
     private OrderService orderService;
 
+    Address a1;
+    Order order1;
+
     @BeforeEach
     void setup() {
+        a1 = new Address();
+        a1.setStreet("Mekelweg 5");
+        a1.setCity("Delft");
+        a1.setCountry("Netherlands");
+        a1.setZip("2628CC");
+
+        order1 = new Order();
+        order1.setOrderID(UUID.randomUUID());
+        order1.setVendorID(UUID.randomUUID());
+        order1.setCustomerID(UUID.randomUUID());
+        order1.setAddress(a1);
+        order1.setDate(new BigDecimal("1700006405000"));
+        order1.setListOfDishes(Arrays.asList(UUID.randomUUID(), UUID.randomUUID()));
+        order1.setSpecialRequirements("Knock on the door");
+        order1.setOrderPaid(true);
+        order1.setStatus(Order.StatusEnum.ACCEPTED);
+        order1.setRating(4);
 
     }
 
@@ -49,6 +72,27 @@ class OrderServiceTests {
         boolean isUnique = orderService.checkUUIDIsUnique(nonExistingUUID);
 
         Assertions.assertFalse(isUnique);
+    }
+
+    @Test
+    void testCreateOrderSuccessful() throws OrderIdAlreadyInUseException {
+
+        Mockito.when(orderRepository.save(order1)).thenReturn(order1);
+
+        Order savedOrder = orderService.createOrder(order1);
+
+        Assertions.assertEquals(savedOrder, order1);
+
+    }
+
+    @Test
+    void testCreateOrderIdTaken() throws OrderIdAlreadyInUseException {
+
+        UUID takenId = order1.getOrderID();
+        Mockito.when(orderService.checkUUIDIsUnique(takenId)).thenReturn(true);
+
+        Assertions.assertThrows(OrderIdAlreadyInUseException.class, () -> orderService.createOrder(order1));
+
     }
 
     @Test
