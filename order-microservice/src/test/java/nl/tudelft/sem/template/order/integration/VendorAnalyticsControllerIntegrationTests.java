3b4e8a4f1da0081486195d0122462a20800a5c12
Thijs Houben <t.h.j.houben@student.tudelft.nl>
2024-01-07 14:48:28 +0100

    Add one of the tests back

--- order-microservice/src/test/java/nl/tudelft/sem/template/order/integration/VendorAnalyticsControllerIntegrationTests.java
+++ order-microservice/src/test/java/nl/tudelft/sem/template/order/integration/VendorAnalyticsControllerIntegrationTests.java
@@ -323,41 +323,41 @@ public class VendorAnalyticsControllerIntegrationTests {
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
-//    /**
-//        This test contains testing using functionality of the method
-//        This is not ideal, but I am not sure how else to approach it
-//     */
-//    @Test
-//    @Transactional
-//    public void get_peak_times_large_test() throws Exception {
-//        dishService.addDish(d1);
-//        dishService.addDish(d2);
-//        order2.setVendorID(UUID.randomUUID());
-//        Random r = new Random();
-//        int[] time = new int[24];
-//        for(int i = 0; i<1000; i++){
-//            long cur = r.nextInt();
-//            order1.setOrderID(UUID.randomUUID());
-//            order1.setDate(BigDecimal.valueOf(cur));
-//            orderService.createOrder(order1);
-//            Calendar calendar = Calendar.getInstance();
-//            calendar.setTimeInMillis(cur);
-//            int hours = calendar.get(Calendar.HOUR_OF_DAY);
-//            time[hours]++;
-//            if(i%10==1){
-//                order2.setOrderID(UUID.randomUUID());
-//                order2.setDate(BigDecimal.valueOf(r.nextInt()));
-//                orderService.createOrder(order2);
-//            }
-//        }
-//        MvcResult res = mockMvc.perform(MockMvcRequestBuilders.get("/vendor/{vendorID}/analytics/peakTimes",order1.getVendorID())
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON)).andReturn();
-//        List<Integer> volume = objectMapper.readValue(res.getResponse().getContentAsString(),new TypeReference<List<Integer>>() {});
-//        assertThat(volume).isEqualTo(Arrays.stream(time).boxed().collect(Collectors.toList()));
-//    }
+    /**
+        This test contains testing using functionality of the method
+        This is not ideal, but I am not sure how else to approach it
+     */
+    @Test
+    @Transactional
+    public void get_peak_times_large_test() throws Exception {
+        dishService.addDish(d1);
+        dishService.addDish(d2);
+        order2.setVendorID(UUID.randomUUID());
+        Random r = new Random();
+        int[] time = new int[24];
+        for(int i = 0; i<1000; i++){
+            long cur = r.nextInt();
+            order1.setOrderID(UUID.randomUUID());
+            order1.setDate(BigDecimal.valueOf(cur));
+            orderService.createOrder(order1);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(cur);
+            int hours = calendar.get(Calendar.HOUR_OF_DAY);
+            time[hours]++;
+            if(i%10==1){
+                order2.setOrderID(UUID.randomUUID());
+                order2.setDate(BigDecimal.valueOf(r.nextInt()));
+                orderService.createOrder(order2);
+            }
+        }
+        MvcResult res = mockMvc.perform(MockMvcRequestBuilders.get("/vendor/{vendorID}/analytics/peakTimes",order1.getVendorID())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON)).andReturn();
+        List<Integer> volume = objectMapper.readValue(res.getResponse().getContentAsString(),new TypeReference<List<Integer>>() {});
+        assertThat(volume).isEqualTo(Arrays.stream(time).boxed().collect(Collectors.toList()));
+    }
 
     @Test
     @Transactional
